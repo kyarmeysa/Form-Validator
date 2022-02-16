f12ee1620ee7292691f3a6b4f5ca35379561d8b0
Rumeysa Kaskaya <kaskayarumeysa@gmail.com>
2022-02-16 12:51:12 +0300

    Some Arrangements were done

--- script.js
+++ script.js
@@ -1,55 +1,45 @@
-const form =document.getElementById('form');
-const username =document.getElementById('username');
-const email =document.getElementById('email');
-const password =document.getElementById('password');
-const repassword =document.getElementById('repassword');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const repassword = document.getElementById('repassword');
 
 
-function Error(input, message){
+function Error(input, message) {
     input.classList.add('is-invalid');
     const div = input.nextElementSibling;
     div.innerText = message;
     div.className = 'invalid-feedback';
 }
-function Success( input){
+function Success(input) {
     input.classList.add('is-valid');
 }
-const validateEmail = (email) => {// i took this code from stackoverflow
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
-    
+function CheckEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test(input.value)) {
+        Success(input);
+    } else {
+        Error(input, 'You  Entered an Incorrect E-mail. Please Enter Your E-Mail Correctly!');
+    }
+}
+
+function CheckRequired(inputs){
+    inputs.forEach(function(input){
+        if(input.value=== ''){
+            Error(input, `${input.id} is required!`);
+        }else{
+            Success(input);
+        }
+    })
+   
+}
 
 
-form.addEventListener('submit', function(e){
+
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    if(username.value===''){
-       Error(username , 'Username Required!');
-    }else{
-        Success(username);
-    }
-    if(email.value===''){
-        Error(email , 'Email Required!');
-    }else if(!validateEmail(email)){
-        Error(email ,'You Should Enter Your E-Mail Corretcly!')
 
-    }
-    else{
-        Success(email);
-    }
-    if(password.value===''){
-        Error(password , 'Password Required!');
-    }else{
-        Success(password);
-    }
-    if(repassword.value===''){
-        Error(repassword , 'Re-password Required!')
-    }else{
-        Success(repassword);
-    }
-
-
-})
+   CheckRequired([username , email , password , repassword]);
+   CheckEmail(email);
+});
